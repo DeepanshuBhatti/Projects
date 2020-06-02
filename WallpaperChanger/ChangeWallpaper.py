e829e0389df6440ac3be680b0c3eeba4be1de0a1
Deepanshu Bhatti <deepanshubhatti@gmail.com>
2020-06-02 14:04:06 +0530

    WPC | 1920x768 default with PyQT5

--- WallpaperChanger/ChangeWallpaper.py
+++ WallpaperChanger/ChangeWallpaper.py
@@ -1,89 +1,120 @@
 import os
+import sys
 import ctypes
+import json
+import logging
 import random
 import threading
 import time
-import PIL.Image
+from PIL import Image
 from os import listdir
 from os.path import isfile, join
-from tkinter import *
-import tkinter.font as font
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
 
-SLEEP_TIME_SECONDS = 180
-WIDTH = 1366
-HEIGHT = 768
-SPI_SETDESKWALLPAPER = 20
-wallpaper_directory = r'E:\Wallpapers\Wallpapers'
+logging.basicConfig(
+    format='[%(asctime)s] %(process)d [%(pathname)s:%(lineno)d | %(levelname)s] - %(''message)s',
+    level=logging.INFO
+)
 
 
-def get_wallpapers():
-    files_list = [f for f in listdir(wallpaper_directory) if isfile(join(wallpaper_directory, f))]
-    return files_list
+class WallpaperChangerApp(QMainWindow):
+    SPI_SETDESKWALLPAPER = 20
+    config_path = './config.json'
+    wallpapers_list = []
+    backup_list = []
 
+    def __init__(self):
+        try:
+            super().__init__()
+            if not os.path.isfile(self.config_path):
+                raise Exception("Config File Not found")
 
-def get_wallpaper(wallpapers_list):
-    file_name = random.choice(wallpapers_list)
-    wallpaper = os.path.join(wallpaper_directory, file_name)
-    return wallpaper
+            with open(self.config_path) as json_data_file:
+                data = json.load(json_data_file)
+            self.wall_height = data["size"]["height"]
+            self.wall_width = data["size"]["width"]
+            self.min_height = data["size"]["min_height"]
+            self.min_width = data["size"]["min_width"]
+            self.wallpaper_directory = data["path"]["wallpaper_directory"]
+            self.auto_change_after = data["auto_change"]["time"]
+            self.wallpapers_list = self.get_wallpaper_path_list()
+            self.init_ui()
+        except Exception as e:
+            logging.error(str(e))
 
+    def init_ui(self):
+        self.setWindowTitle("Changer")
+        self.setFixedHeight(30)
+        self.setFixedWidth(120)
+        self.start_auto_changer()
+        self.add_next_button()
+        self.show()
 
-def change_wallpaper_in_time(list_of_wallpapers):
-    while True:
-        change_desktop_wallpaper(list_of_wallpapers)
-        time.sleep(SLEEP_TIME_SECONDS)
+    def add_next_button(self):
+        next_button = QPushButton(self)
+        next_button.setText("Next")
+        next_button.setFixedHeight(30)
+        next_button.setFixedWidth(120)
+        next_button.clicked.connect(self.next_click)
 
+    def next_click(self):
+        try:
+            self.change_desktop_wallpaper()
+        except Exception as e:
+            logging.error(str(e))
 
-def change_desktop_wallpaper(list_of_wallpapers):
-    wallpaper = get_wallpaper(list_of_wallpapers)
-    wallpaper = resize_wallpaper(wallpaper)
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, wallpaper, 0)
+    def change_desktop_wallpaper(self):
+        wallpaper = self.get_wallpaper()
+        wallpaper = self.resize_wallpaper(wallpaper)
+        ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, wallpaper, 0)
 
-
-def resize_wallpaper(wallpaper):
-    new_path = 'temp.jpg'
-    image_obj = PIL.Image.open(wallpaper)
-
-    while image_obj.size[0] < WIDTH or image_obj.size[1] < HEIGHT:
-        wallpaper = get_wallpaper(list_of_wallpapers)
-        image_obj = PIL.Image.open(wallpaper)
-
-    if image_obj.size[0] == WIDTH or image_obj.size[1] == HEIGHT:
+    def get_wallpaper(self):
+        if len(self.wallpapers_list) < 1:
+            logging.info("Restarting List of Wallpapers")
+            self.wallpapers_list = self.backup_list
+            self.backup_list = []
+        file_name = random.choice(self.wallpapers_list)
+        logging.info("Found " + str(len(self.wallpapers_list)) + " Wallpapers, currently showing " + file_name)
+        self.backup_list.append(file_name)
+        self.wallpapers_list.remove(file_name)
+        wallpaper = os.path.join(self.wallpaper_directory, file_name)
         return wallpaper
 
-    image_obj = image_obj.resize((WIDTH, HEIGHT), PIL.Image.ANTIALIAS)
-    new_path = join(wallpaper_directory, new_path)
-    image_obj.save(new_path)
-    return new_path
+    def resize_wallpaper(self, wallpaper):
+        image_obj = Image.open(wallpaper)
+        width = image_obj.size[0]
+        height = image_obj.size[1]
+        if width == self.wall_width or height == self.wall_height:
+            return wallpaper
+        elif width == self.min_width or height == self.min_height:
+            return self.resize_min_width(image_obj)
+        else:
+            wallpaper = self.get_wallpaper()
+            return self.resize_wallpaper(wallpaper)
 
+    def resize_min_width(self, image_obj):
+        new_path = 'temp.jpg'
+        image_obj = image_obj.resize((self.wall_width, self.wall_height), Image.ANTIALIAS)
+        new_path = join(self.wallpaper_directory, new_path)
+        image_obj.save(new_path)
+        return new_path
 
-def populate_main_window(main_window):
-    myFont = font.Font(family='Calibri', size=20, weight='bold')
-    button = Button(main_window,
-                    text='Next',
-                    height=1,
-                    width=12,
-                    command=lambda: change_desktop_wallpaper(list_of_wallpapers),
-                    bg='gold',
-                    justify='center',
-                    relief=RAISED
-                    )
-    button['font'] = myFont
-    button.grid(column=1, row=3)
+    def get_wallpaper_path_list(self):
+        files_list = [f for f in listdir(self.wallpaper_directory) if isfile(join(self.wallpaper_directory, f))]
+        return files_list
 
+    def change_wallpaper_in_time(self):
+        while True:
+            self.change_desktop_wallpaper()
+            time.sleep(self.auto_change_after)
 
-def start_auto_changer(list_of_wallpapers):
-    thread = threading.Thread(target=change_wallpaper_in_time, args=([list_of_wallpapers]))
-    thread.daemon = True
-    thread.start()
+    def start_auto_changer(self):
+        thread = threading.Thread(target=self.change_wallpaper_in_time, args=())
+        thread.daemon = True
+        thread.start()
 
 
 if __name__ == '__main__':
-    list_of_wallpapers = get_wallpapers()
-    start_auto_changer(list_of_wallpapers)
-    main_window = Tk()
-    main_window.title('Deepanshu Wallpaper Changer')
-    main_window.resizable(False, False)̥̥
-    canvas = Canvas(main_window)
-    populate_main_window(canvas)
-    canvas.pack()
-    main_window.mainloop()
+    app = QApplication(sys.argv)
+    ex = WallpaperChangerApp()
+    sys.exit(app.exec_())
--- WallpaperChanger/README.md
+++ WallpaperChanger/README.md
@@ -12,12 +12,12 @@ This is basically developed for non activated windows as the windows feature to 
 
 ## Features
 
-* Automatically change wallpapers randomly after a given interval (Default: 180s)
+* Automatically change wallpapers randomly after a given interval
 * Gives a UI with *NEXT* button to change wallpaper on wish
 * Both above runs together on different threads
+* Height, width, Min Height, Min width, Wallpaper path and change time are configurable through config.json
 * Wallpaper Directory 'E:\Wallpapers\Wallpapers' by default
-* By default size of wallpaper is 1366x768 pixels
-* You can edit python code for changing time interval, wallpaper directory and size of wallpaper
+* By default size of wallpaper is 1920x768 pixels
 
 ---
 
@@ -29,5 +29,12 @@ This is basically developed for non activated windows as the windows feature to 
   * pyinstaller --onefile -w ChangeWallpaper.py
 * This will generate an exe file in dist folder
 * Put this exe file in Startup folder to run this application on start
+
+---
+
+## Run on Startup
+
+* Paste the generated WallpaperChanger.exe at "C:\Users\<UserName>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+* Restart your computer
 
 ---
